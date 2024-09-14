45b6bf0d421ddc854380fa21e423990a55ce8bc1
Андрей <zz11926557@gmail.com>
2024-09-14 13:02:14 +0700

    [feature] add trainer and exercise

---
+++ backend/Health.Domain/Models/Entities/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using Health.Domain.Interfaces;
+
+namespace Health.Domain.Models.Entities;
+
+public class Exercise : IEntity
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int CaloriesBurned { get; set; }
+    public Trainer Trainer { get; set; }
+    public long TrainerId { get; set; }
+}
+

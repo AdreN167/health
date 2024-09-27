10d4d22a0943a8742f5323a4bc4f4bba1de65829
Андрей <zz11926557@gmail.com>
2024-09-27 21:44:42 +0700

    [bugfix] fix uploads file paths and creating of image file path

--- backend/Health.Core/Features/Products/Commands/Create/CreateProductCommandHandler.cs
+++ backend/Health.Core/Features/Products/Commands/Create/CreateProductCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Health.Core.Resources;
 using Health.DAL;
+using Health.Domain.Models.Common;
 using Health.Domain.Models.Entities;
 using Health.Domain.Models.Enums;
 using Health.Domain.Models.Response;
@@ -39,8 +40,8 @@ public class CreateProductCommandHandler(ApplicationDbContext context)
 
             if (request.Image != null)
             {
-                var fileName = $"product-{newProduct.Id + 1}.{request.Image.FileName.Split('.')[1]}";
-                var filePath = Path.Combine(@"wwwroot\uploads\products", fileName);
+                var fileName = $"product-{Guid.NewGuid()}-{request.Image.FileName}";
+                var filePath = Path.Combine(Constants.PRODUCTS_FOLDER, fileName);
 
                 using (var stream = new FileStream(filePath, FileMode.Create))
                 {
@@ -48,6 +49,10 @@ public class CreateProductCommandHandler(ApplicationDbContext context)
                 }
 
                 newProduct.FileName = fileName;
+            }
+            else
+            {
+                newProduct.FileName = string.Empty;
             }
 
             await context.Products.AddAsync(newProduct);
